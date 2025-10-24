7a03b4bcbecd87607afb2d9cd9d8228912ed38e7
Walter Orellana <walterorellana@WMacBook-Pro.cshl.edu>
2025-10-24 14:44:12 -0400

    random tinkering

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 
 import pygame
@@ -16,6 +18,13 @@ text = font.render("Immune Battle", True, (123, 252, 3))
 textRect = text.get_rect()
 textRect.center = (width // 2, height // 2)
 
+text2 = font.render("Choose your character", True, (123, 252, 3))
+textRect2 = text2.get_rect()
+textRect2.center = (width // 2, height // 10)
+
+object_image = pygame.image.load('virus.png')
+object_rect = object_image.get_rect(center=(width // 2, height // 2))
+
 start_screen = True
 while start_screen:
     screen.fill((0, 0, 0))
@@ -31,6 +40,25 @@ while start_screen:
     pygame.display.flip()
     fpsClock.tick(fps)
 
+second_screen = True
+while second_screen:
+    screen.fill((250, 250, 250))
+    screen.blit(text2, textRect2)
+    screen.blit(object_image, object_rect)
+
+    for event in pygame.event.get():
+            if event.key == pygame.K_r:
+                pygame.quit()
+                
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            second_screen = False
+
+    pygame.display.flip()
+    fpsClock.tick(fps)
 
 # Game loop.
 while True:
