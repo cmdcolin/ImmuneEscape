eb77d20f3e9b7bb736dcda7ccfbb49e8b70bda84
Emma Saart <emmasaart@MacBook-Air-3.cshl.edu>
2025-10-25 20:45:22 -0400

    fixed dictionary fxn

--- dict_of_dict.py
+++ dict_of_dict.py
@@ -6,18 +6,19 @@ def create_dict_of_dict(filename, top_key):
     with open(filename, 'r') as file_read:
         lines = file_read.read().strip().split('\n')
         header = lines[0].split('\t')
+        top_key = header[0]
         for line in lines[1:]:
             #isolate the lines into a list separated by the tabs
             parts = line.strip().split('\t')
             #combine the header line with the components into a dictionary
             data = dict(zip(header,parts))
-            #set top level key
-            key = data['Immune_Name']
             #build the final dictionary
-            result[key] = {
+            result[top_key] = {
                 'Action': data['Action'].split(','),
                 'Damage': [int(x) for x in data['Damage'].split(',')],
                 'Health': [int(data['Health'])],
                 'Image': [data['Image']],
                 'Name': [data['Name']] }
     return result
+
+print(create_dict_of_dict('pathogen.txt','Pathogen_Name'))
