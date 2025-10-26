e8a9ebd56f80ea68a34619bce922de14d82cd3a5
Emma Saart <emmasaart@MacBook-Air-3.cshl.edu>
2025-10-25 20:54:03 -0400

    fixed this time?

--- dict_of_dict.py
+++ dict_of_dict.py
@@ -6,14 +6,17 @@ def create_dict_of_dict(filename, top_key):
     with open(filename, 'r') as file_read:
         lines = file_read.read().strip().split('\n')
         header = lines[0].split('\t')
-        top_key = header[0]
+        id_col = header[0]
         for line in lines[1:]:
             #isolate the lines into a list separated by the tabs
             parts = line.strip().split('\t')
             #combine the header line with the components into a dictionary
             data = dict(zip(header,parts))
+            #set top level keys
+            key = data[id_col].strip()
+            print(key)
             #build the final dictionary
-            result[top_key] = {
+            result[key] = {
                 'Action': data['Action'].split(','),
                 'Damage': [int(x) for x in data['Damage'].split(',')],
                 'Health': [int(data['Health'])],
